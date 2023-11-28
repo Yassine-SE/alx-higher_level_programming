7b71192a801e31ebe0e389f9940be8b60c066b58
Yassine-SE <ytarkaoui@gmail.com>
2023-11-28 21:39:29 +0100

    task 1

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i <= 9:
-        print("0{:d}".format(i) + i)
+        print("0{:d}".format(i) + i, end=", ")
     else:
-        print("{:d}".format(i))
+        print("{:d}".format(i), end=", ")
