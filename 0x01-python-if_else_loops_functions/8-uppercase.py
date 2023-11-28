eb2266ba964549948d0982d405b6659e514e2c7b
Yassine-SE <ytarkaoui@gmail.com>
2023-11-29 00:18:16 +0100

    Task 8.3

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -8,4 +8,6 @@ def uppercase(str):
     for i in str:
         if islower(i):
             print("{:c}".format(ord(i) - 32), end="")
+        else:
+            print("{:c}".format(ord(i)), end="")
     print("")
