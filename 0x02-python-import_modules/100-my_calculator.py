619cbf9e1adca8f25f341bb5b7041dfe74fdad2e
Yassine-SE <ytarkaoui@gmail.com>
2023-11-30 16:47:03 +0100

    linter fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,18 +4,24 @@ if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
     args = sys.argv
     args_len = len(args)
+    a = 0
+    b = 0
+    op = ''
     result = 0
     if args_len != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif args[2] == '+':
-        print("{:s} {:s} {:s} = {:d}".format(args[1], args[2], args[3], add(int(args[1]), int(args[3]))))
+    a = args[1]
+    b = args[1]
+    op = args[2]
+    if args[2] == '+':
+        print("{:s} {:s} {:s} = {:d}".format(a, op, b, add(int(a), int(b))))
     elif args[2] == '-':
-        print("{:s} {:s} {:s} = {:d}".format(args[1], args[2], args[3], sub(int(args[1]), int(args[3]))))
+        print("{:s} {:s} {:s} = {:d}".format(a, op, b, sub(int(a), int(b))))
     elif args[2] == '*':
-        print("{:s} {:s} {:s} = {:d}".format(args[1], args[2], args[3], mul(int(args[1]), int(args[3]))))
+        print("{:s} {:s} {:s} = {:d}".format(a, op, b, mul(int(a), int(b))))
     elif args[2] == '/':
-        print("{:s} {:s} {:s} = {:d}".format(args[1], args[2], args[3], div(int(args[1]), int(args[3]))))
+        print("{:s} {:s} {:s} = {:d}".format(a, op, b, div(int(a), int(b))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
