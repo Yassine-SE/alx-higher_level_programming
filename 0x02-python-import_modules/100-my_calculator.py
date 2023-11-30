71cbcf11d9ef6c2afea007febe02bbb0632e77a0
Yassine-SE <ytarkaoui@gmail.com>
2023-11-30 16:41:17 +0100

    Task 100.2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,13 +9,13 @@ if __name__ == '__main__':
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif args[2] == '+':
-        print("{:d} {:c} {:d} = {:d}".format(args[1], args[2], args[3], add(int(args[1]), int(args[3]))))
+        print("{:s} {:s} {:s} = {:d}".format(args[1], args[2], args[3], add(int(args[1]), int(args[3]))))
     elif args[2] == '-':
-        print("{:d} {:c} {:d} = {:d}".format(args[1], args[2], args[3], sub(int(args[1]), int(args[3]))))
+        print("{:s} {:s} {:s} = {:d}".format(args[1], args[2], args[3], sub(int(args[1]), int(args[3]))))
     elif args[2] == '*':
-        print("{:d} {:c} {:d} = {:d}".format(args[1], args[2], args[3], mul(int(args[1]), int(args[3]))))
+        print("{:s} {:s} {:s} = {:d}".format(args[1], args[2], args[3], mul(int(args[1]), int(args[3]))))
     elif args[2] == '/':
-        print("{:d} {:c} {:d} = {:d}".format(args[1], args[2], args[3], div(int(args[1]), int(args[3]))))
+        print("{:s} {:s} {:s} = {:d}".format(args[1], args[2], args[3], div(int(args[1]), int(args[3]))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
