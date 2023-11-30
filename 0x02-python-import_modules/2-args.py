3b45de8e020f6cf891740d28f2ae71e403a0521d
Yassine-SE <ytarkaoui@gmail.com>
2023-11-30 15:05:11 +0100

    Task2: add colon to output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if __name__ == '__main__':
     if args_len == 1:
         print("0 arguments.")
     elif args_len == 2:
-        print("{:d} argument.".format(args_len - 1))
+        print("{:d} argument:".format(args_len - 1))
         print("{:d}: {:s}".format((args_len - 1), args[1]))
     elif args_len > 2:
-        print("{:d} arguments.".format(args_len - 1))
+        print("{:d} arguments:".format(args_len - 1))
         for i in range(1, args_len):
-            print("{:d}: {:s}".format((i), args[i]))
+            print("{:d}: {:s}".format(i, args[i]))
