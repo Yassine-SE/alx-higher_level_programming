64d339e75a73ff9b6d91eaff8f031a13d1b6fa07
Yassine-SE <ytarkaoui@gmail.com>
2023-12-06 20:42:41 +0100

    Task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new_set = []
-    prv = ""
-    result = 0
-    for i in set_1:
-        new_set.append(i)
-    for b in set_2:
-        new_set.append(b)
-    new_set = new_set.sort()
-    for x in range(len(new_set)):
-        if new_set[x] == new_set[x + 1]:
-            new_set.remove(new_set[x])
-            new_set.remove(new_set[x + 1])
-            x = x + 2
+    return set_1 ^ set_2
