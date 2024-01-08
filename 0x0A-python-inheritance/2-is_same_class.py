d7dfa3ffb4e7a7326707ff600e7e16732aa12a33
Yassine-SE <ytarkaoui@gmail.com>
2024-01-08 16:09:05 +0100

    task 4

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""object"""
+
+
+def is_same_class(obj, a_class):
+    """content"""
+    if type(obj) == a_class:
+        return True
+    return False
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""object"""
+
+
+def is_kind_of_class(obj, a_class):
+    """content"""
+    if isinstance(obj, a_class):
+        return True
+    return False
